e61e2026ee630ea8f6dd6b14a6697b80fc25383a
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2023-09-09 18:05:29 -0300

    adicionando o codigo na classe BancoOrganizadorAtivoS

--- src/ex/Lucas/BancoOrganizadorAtivos/BancoOrganizadorAtivos.java
+++ src/ex/Lucas/BancoOrganizadorAtivos/BancoOrganizadorAtivos.java
@@ -1,4 +1,30 @@
 package ex.Lucas.BancoOrganizadorAtivos;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class BancoOrganizadorAtivos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList < String > ativos = new ArrayList < > ();
+
+        //Entrada dos tipos de ativos
+        int quantidadeAtivos = scanner.nextInt();
+        scanner.nextLine();
+
+
+        // Entrada dos códigos dos ativos
+        for (int i = 0; i < quantidadeAtivos; i++) {
+            String codigoAtivo = scanner.nextLine();
+            ativos.add(codigoAtivo);
+        }
+
+        //TODO: Ordenar os ativos em ordem alfabética.
+        Collections.sort(ativos);
+
+        //TODO: Imprimir a lista de ativos ordenada, conforme a tabela de exemplos.
+        for (String elemento : ativos) {
+            System.out.println(elemento);
+        }
+    }
 }
